ab0b60412f2898d129b7fda6182aee4b375a9ac4
ahmeddi <ah.diyana@gmail.com>
2024-01-20 06:00:41 +0000

    fix bulltins

--- lang/fr/mats.php
+++ lang/fr/mats.php
@@ -4,11 +4,11 @@ return [
 
     'mats' => 'Matières',
 
-    'add' => 'Ajouter une matière', 
+    'add' => 'Ajouter une matière',
     'edit' => 'Modifier le nom de la matière',
-    'nom' => 'Nom de la matière', 
+    'nom' => 'Nom de la matière',
 
-    'add-nom' => 'Nom de la matière', 
+    'add-nom' => 'Nom de la matière',
 
     'mat-cancel' => 'Annuler',
     'mat-save' => 'Enregistrer',
@@ -19,8 +19,11 @@ return [
     'mat-supp' => 'Supprimer',
     'mat-ann' => 'Annuler',
 
+    'mat-lang' => 'Langue',
 
 
 
- 
+
+
+
 ];
